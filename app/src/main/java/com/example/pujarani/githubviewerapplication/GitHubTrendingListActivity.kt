2af96d8006f66592db80518f2cae3db901f0b41b
Puja Rani <Puja.Rani@in.ey.com>
2019-12-03 11:53:18 +0530

    Creating Application GitHubViewerApplication

---
+++ app/src/main/java/com/example/pujarani/githubviewerapplication/GitHubTrendingListActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pujarani.githubviewerapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class GitHubTrendingListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_git_hub_trending_list)
+    }
+}
